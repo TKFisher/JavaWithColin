7d6d288aac3fd91cd214c0b9385986f0631c30cb
Tracy Fisher <tfisher@sbgtv.com>
2018-04-14 23:13:49 -0700

    add Echo exercise

---
+++ src/HeadFirstJava/Chapter_2/Echo.java
@@ -0,0 +1,12 @@
+package HeadFirstJava.Chapter_2;
+
+public class Echo
+{
+
+    int count = 0;
+
+    public void hello () {
+        System.out.println("helloooo... ");
+
+    }
+}
--- src/HeadFirstJava/Chapter_2/Exercises.java
+++ src/HeadFirstJava/Chapter_2/Exercises.java
@@ -6,7 +6,7 @@ public class Exercises
     public static void main(String[] args)
     {
         // Pool Puzzle
-        
+
 
     }
 
