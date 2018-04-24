7bc4b5bb5d9297fe6a5261cc93fce1877789adb4
Tracy Fisher <tfisher@sbgtv.com>
2018-04-24 14:46:45 -0700

    changes to Triangle class..works now.

--- src/HeadFirstJava/Chapter_3/Triangle.java
+++ src/HeadFirstJava/Chapter_3/Triangle.java
@@ -10,36 +10,48 @@ public class Triangle
 
     public static void main(String[] args)
     {
-
+        // Creates an object array called Triangle and assigns with 4 index positions in the array.
         Triangle[] ta = new Triangle[4];
 
-        ta[0] = new Triangle();
-        ta[1] = new Triangle();
-        ta[2] = new Triangle();
-        ta[3] = new Triangle();
-
+        // variable 'x' initialized to zero.
         int x = 0;
-
 
         while (x < 4) {
 
-            ta[x].setArea();
-
+            // Creates the Triangle object and assigns 'ta[x]' with 3 empty variables: area, height, length
+            // Each pass of the loop will store the values for area, height, and length in the index positions
+            // of the array.
+            ta[x] = new Triangle();
             ta[x].height = (x + 1) * 2;
             ta[x].length = x + 4;
 
-            //
-
+            // The method setArea is applied using the values stored in 'ta[x]'
+            ta[x].setArea();
             System.out.print("triangle " + x + ", area");
-            System.out.println(" = " +  ta[x].area);
+            System.out.println(" = " + ta[x].area);
 
+            // incrementer to allow for looping
             x = x + 1;
         }
 
+        // takes the value in 'x' and assignes it to 'y'
+        int y = x;
+        x = 27;
+
+        // Creates a local variable of the Triangle Class and assigns the values held in the last array of the loop,
+        // index position 2 of the 'ta' array to the variable 't5'. 't5' will contain all 3 variables.
+        Triangle t5 = ta[2];
+
+        // The single value of '343' is assigned to the 'area' variable in the 2 array.
+        ta[2].area = 343;
+        System.out.print("y = " + y);
+        System.out.println(", t5 area = " + t5.area);
+
     }
 
-    void setArea()
+    // Method to calculate area.
+    public void setArea()
     {
-        area = (height * length) / 2;
+        area = (length / 2) * height;
     }
 }
